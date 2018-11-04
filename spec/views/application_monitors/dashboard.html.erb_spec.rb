65e746e9cf1993776ffbca16b939bf860dc447d6
Scott Goyette <sgoyette@kerberosfoundation.com>
2018-11-03 23:41:19 -0400

    updated spec and modified css

--- spec/views/application_monitors/dashboard.html.erb_spec.rb
+++ spec/views/application_monitors/dashboard.html.erb_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe "application_monitors/dashboard.html.erb", type: :view do
   before do
-    @monitors = ApplicationMonitor.all
+    @monitors = ApplicationMonitor.order(:application_name).page(params[:id])
   end
   it 'renders Application Dashboard' do
     render :template => 'application_monitors/dashboard.html.erb'
-    expect(rendered).to match /Application Dashboard/
+    expect(rendered).to match /Currently Monitoring Applications/
   end
 end
