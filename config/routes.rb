7815293643f59d76d62a227cc2b061ab2bd6f8ae
Scott Goyette <sgoyette@kerberosfoundation.com>
2018-11-13 19:47:06 -0500

    navigation fixes, routing, gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,18 @@ Rails.application.routes.draw do
   resources :application_monitors, path: 'am'
   resources :subscribers
   root to: 'subscribers#index'
+
+  require "sidekiq/web"
+  require 'sidekiq/cron/web'
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    ActiveSupport::SecurityUtils.secure_compare(
+      ::Digest::SHA256.hexdigest(username),
+      ::Digest::SHA256.hexdigest(ENV["SK_USER"])
+    ) &
+    ActiveSupport::SecurityUtils.secure_compare(
+      ::Digest::SHA256.hexdigest(password),
+      ::Digest::SHA256.hexdigest(ENV["SK_PASS"])
+    )
+  end if Rails.env.production?
+  mount Sidekiq::Web, at: "/sidekiq"
 end
